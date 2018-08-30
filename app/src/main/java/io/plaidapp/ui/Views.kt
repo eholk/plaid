1ff9650ff6e8bc711277ec6f7fe741b725306544
Eric Holk <eholk@google.com>
2018-08-30 15:32:34 -0700

    Fixed the layout!

--- app/src/main/java/io/plaidapp/ui/Views.kt
+++ app/src/main/java/io/plaidapp/ui/Views.kt
@@ -31,25 +31,30 @@ object Views {
         xml.next() // <FrameLayout>
 //        println(xml.name + " " + attrs.idAttribute)
         val frame = FrameLayout(context, attrs)
+        val frame_params = view.generateLayoutParams(attrs);
 
         xml.next() // <View>
 //        println(xml.name + " " + attrs.getAttributeName(0) + "=" + attrs.getAttributeValue(0) + " :: " + R.id.status_bar_background)
-        frame.addView(View(context, attrs))
+        var layout_params = frame.generateLayoutParams(attrs)
+        frame.addView(View(context, attrs), layout_params)
         xml.next() // </View>
 
         xml.next() // <RecyclerView>
 //        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + R.id.grid)
-        frame.addView(RecyclerView(context, attrs))
+        layout_params = frame.generateLayoutParams(attrs)
+        frame.addView(RecyclerView(context, attrs), layout_params)
         xml.next() // </RecyclerView>
 
         xml.next() // <Toolbar>
 //        println(xml.name + " " + attrs.getAttributeName(0) + "=" + attrs.getAttributeValue(0) + " :: " + R.id.toolbar)
-        frame.addView(Toolbar(context, attrs))
+        layout_params = frame.generateLayoutParams(attrs)
+        frame.addView(Toolbar(context, attrs), layout_params)
         xml.next() // </Toolbar>
 
         xml.next() // <ProgressBar>
 //        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + android.R.id.empty)
-        frame.addView(ProgressBar(context, attrs))
+        layout_params = frame.generateLayoutParams(attrs)
+        frame.addView(ProgressBar(context, attrs), layout_params)
         xml.next() // </ProgressBar>
 
         xml.next() // <ViewStub>
@@ -57,34 +62,39 @@ object Views {
         var stub = ViewStub(context, attrs)
         val inflater = LayoutInflater.from(context)
         stub.layoutInflater = inflater.cloneInContext(context)
-        frame.addView(stub)
+        layout_params = frame.generateLayoutParams(attrs)
+        frame.addView(stub, layout_params)
         xml.next() // </ViewStub>
 
         xml.next() // <ViewStub>
-//        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + R.id.stub_no_connection)
+        //println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + R.id.stub_no_connection)
         stub = ViewStub(context, attrs)
         stub.layoutInflater = inflater.cloneInContext(context)
-        frame.addView(stub)
+        layout_params = frame.generateLayoutParams(attrs)
+        frame.addView(stub, layout_params)
         xml.next() // </ViewStub>
 
         xml.next() // <ImageButton>
 //        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + R.id.fab)
-        frame.addView(ImageButton(context, attrs))
+        layout_params = frame.generateLayoutParams(attrs)
+        frame.addView(ImageButton(context, attrs), layout_params)
         xml.next() // </ImageButton>
 
         xml.next() // <ViewStub>
 //        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + R.id.stub_posting_progress)
         stub = ViewStub(context, attrs)
         stub.layoutInflater = inflater.cloneInContext(context)
-        frame.addView(stub)
+        layout_params = frame.generateLayoutParams(attrs)
+        frame.addView(stub, layout_params)
         xml.next() // </ViewStub>
 
-        view.addView(frame)
+        view.addView(frame, frame_params)
         xml.next() // </FrameLayout>
 
         xml.next() // <RecyclerView>
 //        println(xml.name + " " + attrs.getAttributeName(2) + "=" + attrs.getAttributeValue(2) + " :: " + R.id.filters)
-        view.addView(RecyclerView(context, attrs))
+        val layout_params2 = view.generateLayoutParams(attrs)
+        view.addView(RecyclerView(context, attrs), layout_params2)
 
         // We aren't actually going to read the rest of this parse, so don't call it.
         //xml.next() // </RecyclerView>
