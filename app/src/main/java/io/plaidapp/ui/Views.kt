184bb44b1f54a5f54edf75fb9a55dc1958aa54c3
Eric Holk <eholk@google.com>
2018-08-30 14:21:28 -0700

    Tweak logging

--- app/src/main/java/io/plaidapp/ui/Views.kt
+++ app/src/main/java/io/plaidapp/ui/Views.kt
@@ -2,8 +2,10 @@ package io.plaidapp.ui
 
 import android.content.Context
 import android.support.v4.widget.DrawerLayout
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Xml
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewStub
 import android.widget.*
@@ -18,67 +20,75 @@ object Views {
         // attributes to each constructor.
         val res = context.resources
         val xml = res.getLayout(R.layout.activity_home)
-        val attrs = Xml.asAttributeSet(xml)
 
         xml.next() // <?xml version="1.0" encoding="utf-8"?>
         xml.next() // copyright header
 
-        println(xml.name)
+        val attrs = Xml.asAttributeSet(xml)
+//        println(xml.name + " " + attrs.getAttributeName(0) + "=" + attrs.getAttributeValue(0) + " :: " + R.id.drawer)
         val view = DrawerLayout(context, attrs)
 
         xml.next() // <FrameLayout>
-        println(xml.name)
+//        println(xml.name + " " + attrs.idAttribute)
         val frame = FrameLayout(context, attrs)
 
         xml.next() // <View>
-        println(xml.name)
+//        println(xml.name + " " + attrs.getAttributeName(0) + "=" + attrs.getAttributeValue(0) + " :: " + R.id.status_bar_background)
         frame.addView(View(context, attrs))
         xml.next() // </View>
 
         xml.next() // <RecyclerView>
-        println(xml.name)
+//        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + R.id.grid)
         frame.addView(RecyclerView(context, attrs))
         xml.next() // </RecyclerView>
 
         xml.next() // <Toolbar>
-        println(xml.name)
+//        println(xml.name + " " + attrs.getAttributeName(0) + "=" + attrs.getAttributeValue(0) + " :: " + R.id.toolbar)
         frame.addView(Toolbar(context, attrs))
         xml.next() // </Toolbar>
 
         xml.next() // <ProgressBar>
-        println(xml.name)
+//        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + android.R.id.empty)
         frame.addView(ProgressBar(context, attrs))
         xml.next() // </ProgressBar>
 
         xml.next() // <ViewStub>
-        println(xml.name)
-        frame.addView(ViewStub(context, attrs))
+//        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + R.id.stub_no_filters)
+        var stub = ViewStub(context, attrs)
+        val inflater = LayoutInflater.from(context)
+        stub.layoutInflater = inflater.cloneInContext(context)
+        frame.addView(stub)
         xml.next() // </ViewStub>
 
         xml.next() // <ViewStub>
-        println(xml.name)
-        frame.addView(ViewStub(context, attrs))
+//        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + R.id.stub_no_connection)
+        stub = ViewStub(context, attrs)
+        stub.layoutInflater = inflater.cloneInContext(context)
+        frame.addView(stub)
         xml.next() // </ViewStub>
 
         xml.next() // <ImageButton>
-        println(xml.name)
+//        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + R.id.fab)
         frame.addView(ImageButton(context, attrs))
         xml.next() // </ImageButton>
 
         xml.next() // <ViewStub>
-        println(xml.name)
-        frame.addView(ViewStub(context, attrs))
+//        println(xml.name + " " + attrs.getAttributeName(1) + "=" + attrs.getAttributeValue(1) + " :: " + R.id.stub_posting_progress)
+        stub = ViewStub(context, attrs)
+        stub.layoutInflater = inflater.cloneInContext(context)
+        frame.addView(stub)
         xml.next() // </ViewStub>
 
         view.addView(frame)
         xml.next() // </FrameLayout>
 
         xml.next() // <RecyclerView>
-        println(xml.name)
+//        println(xml.name + " " + attrs.getAttributeName(2) + "=" + attrs.getAttributeValue(2) + " :: " + R.id.filters)
         view.addView(RecyclerView(context, attrs))
 
         // We aren't actually going to read the rest of this parse, so don't call it.
         //xml.next() // </RecyclerView>
+        //xml.next() // </DrawerLayout>
 
         return view
     }
